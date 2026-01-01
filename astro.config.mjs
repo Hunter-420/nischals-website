e66709e8d4e160248d8d0afeabfcc5cf95a1bef7
Nischal Khanal <khanalnischal2075@gmail.com>
2026-01-01 12:18:37 +0545

    Inline styles to fix 404s permanently

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://hunter-420.github.io',
   base: '/nischals-website/', // IMPORTANT: for GitHub Pages project site
+  build: {
+    assets: 'assets', // Renames _astro to assets to avoid GitHub Pages ignoring it
+  },
 });
